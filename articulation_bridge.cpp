8f21f83cdd445cc42ef4e6bf68a03267419bc477
Dustin <dustidusk@gmail.com>
2022-02-10 16:27:12 +0700

    add input output description for dfs

--- articulation_bridge.cpp
+++ articulation_bridge.cpp
@@ -13,13 +13,15 @@ set<int> curVertices;
 vector<pair<int, int>> bridges;
 int vtime;
 
-void input() {
+void input()
+{
   cin >> N >> M;
   adjacentList.resize(N);
   visited.resize(N);
   entryPoint.resize(N);
   lowPoint.resize(N);
-  for (int i = 0; i < M; i++) {
+  for (int i = 0; i < M; i++)
+  {
     int a, b;
     cin >> a >> b;
     adjacentList[a - 1].push_back(b - 1);
@@ -27,48 +29,85 @@ void input() {
   }
 }
 
-void dfs(int v, int p = -1) {
+void dfs(int v, int p = -1)
+{
   visited[v] = true;
   entryPoint[v] = lowPoint[v] = ++vtime;
   int children = 0;
-  for (int to : adjacentList[v]) {
+  for (int to : adjacentList[v])
+  {
     if (p == to)
       continue;
-    if (visited[to]) {
+    if (visited[to])
+    {
       lowPoint[v] = min(lowPoint[v], entryPoint[to]);
-    } else {
+    }
+    else
+    {
       dfs(to, v);
       lowPoint[v] = min(lowPoint[v], lowPoint[to]);
-      if (lowPoint[to] >= entryPoint[v] && p != -1) {
+      if (lowPoint[to] >= entryPoint[v] && p != -1)
+      {
         curVertices.insert(v);
       }
-      if (lowPoint[to] > entryPoint[v]) {
+      if (lowPoint[to] > entryPoint[v])
+      {
         bridges.push_back({v, to});
       }
       children++;
     }
   }
-  if (p == -1 && children > 1) {
+  if (p == -1 && children > 1)
+  {
     curVertices.insert(v);
   }
 }
 
-void traverse() {
+void traverse()
+{
   // reset some data;
   vtime = 0;
   entryPoint.assign(N, -1);
   lowPoint.assign(N, -1);
   visited.assign(N, false);
-  for (int i = 0; i < N; i++) {
-    if (!visited[i]) {
+  for (int i = 0; i < N; i++)
+  {
+    if (!visited[i])
+    {
       dfs(i);
     }
   }
 }
 
-int main() {
+int main()
+{
+  // input:
+  // Dong dau:
+  //    SoDinh  SoCanh
+  // Nhap cac canh:
+  //    Dinh1   Dinh2
   input();
   traverse();
-  cout << curVertices.size() << " " << bridges.size();
+  cout << "Number of Cut vertices: " << curVertices.size() << "\nNumber of Bridges:" << bridges.size();
+  cout << "\nCut vertices: ";
+  for (auto i : curVertices)
+  {
+    cout << i << " ";
+  }
+  cout << "\nBridges: \n";
+  for (auto i : bridges)
+  {
+    cout << i.first << " - " << i.second << endl;
+  }
+  cout << "Low array: ";
+  for (auto i : lowPoint)
+  {
+    cout << i << " ";
+  }
+  cout << "\nNum array: ";
+  for (auto i : entryPoint)
+  {
+    cout << i << " ";
+  }
   return 0;
 }
